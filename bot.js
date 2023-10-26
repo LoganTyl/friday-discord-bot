db6bf46770baf912d3b3382ea2a00122393bd129
LoganTyl <47304609+LoganTyl@users.noreply.github.com>
2023-10-26 16:39:59 -0700

    Got commands to work properly within Discord

--- bot.js
+++ bot.js
@@ -15,16 +15,19 @@ const client = new Client({ intents: [
 client.commands = new Collection();
 
 const commandDirectory = path.join(__dirname, 'commands');
-const commandFiles = fs.readdirSync(commandDirectory).filter(file => file.endsWith('.js'));
+const commandFolders = fs.readdirSync(commandDirectory);
 
-for (const file of commandFiles) {
-    const filePath = path.join(commandDirectory, file);
-    const command = require(filePath);
-    // Set a new item in the Collection with the key as the command name and the value as the exported module
-    if ('data' in command && 'execute' in command) {
-		client.commands.set(command.data.name, command);
-	} else {
-		console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+for (const folder of commandFolders) {
+	const commandsPath = path.join(commandDirectory, folder);
+	const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+	for (const file of commandFiles) {
+		const filePath = path.join(commandsPath, file);
+		const command = require(filePath);
+		if ('data' in command && 'execute' in command) {
+			client.commands.set(command.data.name, command);
+		} else {
+			console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+		}
 	}
 }
 
@@ -59,7 +62,6 @@ client.on(Events.InteractionCreate, async interaction => {
 	if (!interaction.isChatInputCommand()) {
         return;
     }
-        
 
 	const command = interaction.client.commands.get(interaction.commandName);
 
